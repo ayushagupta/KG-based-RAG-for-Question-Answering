266aca016e3e0d1c4ece22525925d2e7f236fe29
Ayush Gupta <mumbai.ayushgupta@gmail.com>
2025-03-18 17:46:11 -0400

    update setup to create logs folder

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from config.config import config
 import os
 from vectorDB.create_vectordb import create_vector_db
 
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+    
+
 try:
     if os.path.exists(config.VECTOR_DB_PATH):
         print("VectorDB already exists.")
