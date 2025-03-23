7b1829422990a2898a3384ba9496cc25529e59cc
Ayush Gupta <mumbai.ayushgupta@gmail.com>
2025-03-22 23:48:25 -0400

    get nodes when no disease entities found

--- rag/rag.py
+++ rag/rag.py
@@ -17,28 +17,30 @@ class RAG:
         question_embedding = get_text_embedding(question, self.embedding_function)
         nodes_found = []
         node_context_extracted = ""
+        max_number_of_high_similarity_context_per_node = 0
 
         if disease_entities:
             max_number_of_high_similarity_context_per_node = int(self.context_volume/len(disease_entities))
             for disease in disease_entities:
                 node_search_result = self.vector_store.similarity_search_with_score(disease, k=1)
                 nodes_found.append(node_search_result[0][0].page_content)
-            
-            for node in nodes_found:
-                node_context, context_table = get_context_from_spoke_api(node)
-
-                relevant_context = extract_relevant_context(
-                    node_context=node_context,
-                    question_embedding=question_embedding,
-                    embedding_function=self.embedding_function,
-                    percentile_threshold=self.context_similarity_percentile_threshold,
-                    min_threshold=self.context_similarity_min_threshold,
-                    max_count=max_number_of_high_similarity_context_per_node
-                )
-
-                node_context_extracted += relevant_context
-
-            return node_context_extracted
-
         else:
-            pass
+            k = 5
+            nodes_found = self.vector_store.similarity_search_with_score(question, k=k)
+            max_number_of_high_similarity_context_per_node = int(self.context_volume/k)
+
+        for node in nodes_found:
+            node_context, context_table = get_context_from_spoke_api(node)
+
+            relevant_context = extract_relevant_context(
+                node_context=node_context,
+                question_embedding=question_embedding,
+                embedding_function=self.embedding_function,
+                percentile_threshold=self.context_similarity_percentile_threshold,
+                min_threshold=self.context_similarity_min_threshold,
+                max_count=max_number_of_high_similarity_context_per_node
+            )
+
+            node_context_extracted += relevant_context
+
+        return node_context_extracted
